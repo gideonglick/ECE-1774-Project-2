a4a7c397b00f9eb7c1e32989eac5392077139a37
mas1057 <156371576+mas1057@users.noreply.github.com>
2026-03-03 08:17:54 -0500

    3/3/26 8:17

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ pd.set_option('display.width', None)
 pd.set_option('display.float_format', lambda x: f'{x:.4f}')
 
 def main():
-    # Case 6.9 - Five Bus System
     case69 = Circuit("Case 6.9")
 
     # Add 5 buses
@@ -17,16 +16,13 @@ def main():
     case69.add_bus("Bus 4", 345.0)
     case69.add_bus("Bus 5", 345.0)
 
-    # Add transformers (Table 6.3)
     case69.add_transformer("T1", "Bus 1", "Bus 5", 0.00150, 0.02)
     case69.add_transformer("T2", "Bus 3", "Bus 4", 0.00075, 0.01)
 
-    # Add transmission lines (Table 6.2)
     case69.add_transmission_line("Line 1", "Bus 2", "Bus 4", 0.0090, 0.100, 0, 1.72)
     case69.add_transmission_line("Line 2", "Bus 2", "Bus 5", 0.0045, 0.050, 0, 0.88)
     case69.add_transmission_line("Line 3", "Bus 4", "Bus 5", 0.00225, 0.025, 0, 0.44)
 
-    # Calculate Ybus
     case69.calc_ybus()
     print(case69.ybus)
 if __name__ == "__main__":
